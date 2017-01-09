6ddfbd92aaa7df3ff6692c4d522d4ddfb7201d56
t_rappos <t_rappos@hotmail.com>
2017-01-09 19:33:32 +1100

    updated to work with newest js

--- react_boilerplate_project/public/bundle.js
+++ react_boilerplate_project/public/bundle.js
@@ -46,8 +46,13 @@
 
 	'use strict';
 
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 	var React = __webpack_require__(1);
 	var ReactDOM = __webpack_require__(158);
+
+	var objOne = { name: 'andrew', location: 'saf' };
+	var objTwo = _extends({ age: 25 }, objOne);
 
 	ReactDOM.render(React.createElement(
 	  'h1',
