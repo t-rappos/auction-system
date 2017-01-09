53fab5089b02852c90afc8e7da342da1a8409524
t_rappos <t_rappos@hotmail.com>
2017-01-09 22:11:34 +1100

    continuing weather tutorial

---
+++ ReactWeather/app/components/Weather.jsx
@@ -0,0 +1,34 @@
+var React = require('react');
+var WeatherForm = require('WeatherForm');
+var WeatherMessage = require('WeatherMessage');
+
+var Weather = React.createClass({
+
+  getInitialState: function(){
+    return {
+      location: 'miami',
+      temp: 88
+    }
+  },
+
+  handleSearch: function(location){
+    this.setState({
+      location: location,
+      temp: 23
+    });
+  },
+
+  render: function(){
+    var {temp, location} = this.state;
+
+    return (
+      <div>
+        <h3>Weather Component</h3>
+        <WeatherForm onSearch={this.handleSearch}/>
+        <WeatherMessage temp={temp} location={location}/>
+      </div>
+    )
+  }
+});
+
+module.exports = Weather;
