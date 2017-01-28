1dda1ba8cdcb5df529ce8184d182781aee8e7783
t_rappos <t_rappos@hotmail.com>
2017-01-28 16:48:14 +1100

    trying to see if null coverage test will pass v2

---
+++ demo.js
@@ -0,0 +1,6 @@
+function add(a,b){
+  if (a===b){return 0;}
+  return a+b;
+}
+
+module.exports = {add:add};
--- test/server.js
+++ test/server.js
@@ -1,14 +1,21 @@
 var expect = require('expect');
 //var server = require('../server.js');
-
+var demo = require('../demo.js');
 
 describe('Server',()=>{
   it('should run properly run test',()=>{
     expect(1).toBe(1);
   });
 
-  //it('should run properly run test',()=>{
-  //  expect(server).toExist();
-  //});
+  it('should demo',()=>{
+    expect(demo).toExist();
+  });
+
+  it('should demo add',()=>{
+    var res = demo.add(1,2);
+    var exp = 3;
+    expect(res).toEqual(exp);
+  });
+
 
 });
