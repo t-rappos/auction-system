577c66b4aab8b9f4b880becddf7fd40b4b1f8942
t_rappos <t_rappos@hotmail.com>
2017-02-09 14:34:46 +1100

    added tests for message and drafted code to pass test

---
+++ test/messageFactory_test.js
@@ -0,0 +1,22 @@
+/*
+var expect = require('expect');
+//let Utility = require('../lib/utility.js');
+let MessageFactory = require('../lib/messageFactory.js');
+describe('User',function(){
+
+  it('should exist',function(done){
+    expect(User).toExist();
+    done();
+  });
+
+});
+*/
+
+/*
+it('should be able to set as read',function(done){
+  let msg = MessageFactory.createMessage('sendertom','recieverandy','title','message contents');
+  msg.setRead();
+  expect(msg.isRead()).toBe(true);
+  done();
+});
+*/
