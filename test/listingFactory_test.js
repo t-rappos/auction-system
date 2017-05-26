d37045eca026849e012651d761a747236d17c805
t_rappos <t_rappos@hotmail.com>
2017-05-26 16:14:43 +1000

    added tests for listingFactory

---
+++ test/listingFactory_test.js
@@ -0,0 +1,189 @@
+let expect = require('expect');
+let ListingFactory = require('../lib/listingFactory.js');
+let AccountFactory = require('../lib/accountFactory.js');
+let ItemFactory = require('../lib/itemFactory.js');
+
+let testItemId = null;
+let testItemId2 = null;
+let testAccountId = null;
+let testListingId = null;
+let testListingId2 = null;
+
+describe('listing factory',function(){
+
+  it("should exist",function(done){
+    expect(ListingFactory).toExist();
+    done();
+  });
+
+  it('should be able to do start up',function(done){
+    AccountFactory.createAccount('lftom', 'lfpassword', 'lftom@gmail.com')
+    .then((account)=>{
+      testAccountId = account.getId();
+      return ItemFactory.createItem('lfTestItemName', 'itemDesc', 'www.itemUrl.com', testAccountId);
+    })
+    .then((item)=>{
+      testItemId = item.getId();
+    })
+    .then(()=>{
+      return ItemFactory.createItem('lfTestItemName2', 'itemDesc2', 'www.itemUrl.com2', testAccountId);
+    })
+    .then((item)=>{
+      testItemId2 = item.getId();
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to add listing bid",function(done){
+    ListingFactory.createListing(testItemId, 100, 3600*24, 'bid')
+    .then((listing)=>{
+      expect(listing).toNotBe(null).toNotBe(undefined);
+      expect(listing.getItem().getId()).toBe(testItemId);
+      expect(listing.getType()).toBe('bid');
+      expect(listing.getStartPrice()).toBe(100);
+      expect(listing.getCreateTime()).toNotBe(null).toNotBe(undefined);
+      expect(listing.getEndTime()).toNotBe(null).toNotBe(undefined);
+      expect(listing.getSellerId()).toBe(testAccountId);
+      expect(listing.getWinnerId()).toBe(null);
+      expect(listing.getId()).toNotBe(null);
+      testListingId = listing.getId();
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+
+  it("should be able to add listing buyout",function(done){
+    ListingFactory.createListing(testItemId2, 100, 3600*24, 'buyout')
+    .then((listing)=>{
+      expect(listing).toNotBe(null).toNotBe(undefined);
+      expect(listing.getItem().getId()).toBe(testItemId);
+      expect(listing.getType()).toBe('buyout');
+      expect(listing.getStartPrice()).toBe(100);
+      expect(listing.getCreationDate()).toNotBe(null).toNotBe(undefined);
+      expect(listing.getExpiryDate()).toNotBe(null).toNotBe(undefined);
+      expect(listing.getSellerId()).toBe(testAccountId);
+      expect(listing.isSold()).toBe(false);
+      expect(listing.getId()).toNotBe(null);
+      testListingId2 = listing.getId();
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("shouldnt be able to list twice",function(done){
+    expect(()=>{
+      ListingFactory.createListing(testItemId2, 100, 3600*24, 'buyout');
+    }).toThrow(/duplicate/);
+    done();
+  });
+
+  it("should be able to get listings",function(done){
+    ListingFactory.getAllListings()
+    .then((listings)=>{
+      expect(listings.length).toBe(2);
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to get account listings",function(done){
+    ListingFactory.getAccountListings(testAccountId)
+    .then((listings)=>{
+      expect(listings.length).toBe(2);
+      return ListingFactory.getAccountListings(-1);
+    })
+    .then((listings)=>{
+      expect(listings.length).toBe(0);
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to cancel listings",function(done){
+    ListingFactory.getAccountListings(testAccountId)
+    .then((listings)=>{
+      expect(listings.length).toBe(2);
+      return ListingFactory.cancelListing(testListingId);
+    })
+    .then(()=>{
+      return ListingFactory.getAccountListings(testAccountId);
+    })
+    .then((listings)=>{
+      expect(listings.length).toBe(1);
+      expect(listings[0].getId()).toBe(testListingId2);
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to cancel all listings",function(done){
+    ListingFactory.cancelAllListings()
+    .then(()=>{
+      return ListingFactory.getAllListings();
+    })
+    .then((listings)=>{
+      expect(listings.length).toBe(0);
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to cancel all user listings",function(done){
+    ListingFactory.createListing(testItemId, 100, 3600*24, 'bid')
+    .then(()=>{
+      return ListingFactory.getAccountListings(testAccountId);
+    })
+    .then((listings)=>{
+      expect(listings.length).toBe(1);
+      return ListingFactory.cancelAccountListings(testAccountId);
+    })
+    .then(()=>{
+      return ListingFactory.getAccountListings(testAccountId);
+    })
+    .then((listings)=>{
+      expect(listings.length).toBe(0);
+      done();
+    })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+
+  it("should be able to do shutdown",function(done){
+     ItemFactory.removeAllItems()
+     .then(()=>{
+       AccountFactory.destroyAllAccounts();
+     })
+     .then(()=>{
+       done();
+     })
+    .catch((e)=>{
+      console.log(e);
+      throw(e);
+    });
+  });
+});
---
+++ test/listing_test.js
@@ -0,0 +1,2 @@
+//let expect = require('expect');
+//let Listing = require('../lib/listing.js');
