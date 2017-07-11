fa4d357880ef37caaaad8daaf39844f8077e07d0
t_rappos <t_rappos@hotmail.com>
2017-07-12 09:43:42 +1000

    added client links to server functionality

--- app/api/server.jsx
+++ app/api/server.jsx
@@ -113,19 +113,33 @@ function sendAccountCreationRequest(username, email, password){
 }
 
 module.exports = {
+  
+  sendListItemBidRequest : (itemId, price, duration, callback)=>{makeRequest('list_item_bid', {itemId : itemId, price : price, duration : duration}, callback);},
+  sendListItemBuyoutRequest : (itemId, price, duration, callback)=>{makeRequest('list_item_buyout', {itemId : itemId, price : price, duration : duration}, callback);},
+  sendCancelListingRequest : (listingId, callback)=>{makeRequest('cancel_listing', {listingId : listingId}, callback);},
+  sendBidOnListingRequest : (listingId, amount, callback)=>{makeRequest('bid_on_listing', {listingId : listingId, amount : amount}, callback);},
+  sendBuyoutListingRequest : (listingId, callback)=>{makeRequest('buyout_listing', {listingId : listingId}, callback);},
+  sendViewAccountListingsRequest : (accountId, callback)=>{makeRequest('view_account_listings', {accountId : accountId}, callback);},
+  sendViewListingsRequest : (callback)=>{makeRequest('view_listings', {}, callback);},
+
   sendAccountIdRequest : (username, callback)=>{makeRequest('get_account_id', {username : username}, callback);},
+  sendAccountCreationRequest : sendAccountCreationRequest,
+  sendAccountModifyRequest : sendAccountModifyRequest,
+  sendAccountViewRequest : sendAccountViewRequest,
+
   sendMessageDeleteRequest  : sendMessageDeleteRequest,
   sendMessageReadRequest  : sendMessageReadRequest,
   sendMessageCreationRequest : sendMessageCreationRequest,
   sendMessageListViewRequest : sendMessageListViewRequest,
-  sendTagViewRequest : sendTagViewRequest,
+
+  sendInventoryViewRequest : sendInventoryViewRequest,
   sendItemCreationRequest : sendItemCreationRequest,
+  sendDestroyItemRequest : (itemId, callback)=>{makeRequest('destroy_item', {itemId : itemId}, callback);},
+
   sendImageListViewRequest : sendImageListViewRequest,
-  sendAccountViewRequest : sendAccountViewRequest,
-  sendUserLoginRequest : sendUserLoginRequest,
-  sendAccountCreationRequest : sendAccountCreationRequest,
-  sendAccountModifyRequest : sendAccountModifyRequest,
-  sendInventoryViewRequest : sendInventoryViewRequest
+  sendTagViewRequest : sendTagViewRequest,
+  sendUserLoginRequest : sendUserLoginRequest
+  
 ////////////////////
 //Public CALLBACKS//
 ////////////////////
